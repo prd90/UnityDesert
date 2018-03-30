2bb43823becaefc5fb2d44611c346514c850e968
prd90 <prerit.datta@ttu.edu>
2018-03-30 09:51:10 -0500

    Initial Commit
    Initial Commit

---
+++ Project2/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDownTimer : MonoBehaviour {
+
+	public int timeLeft = 5;
+	Text countdownText;
+
+	// Use this for initialization
+	void Start()
+	{
+		countdownText = GetComponent<Text> ();
+		StartCoroutine("LoseTime");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		countdownText.text = ("Time Left = " + timeLeft);
+
+		if (timeLeft <= 0)
+		{
+			StopCoroutine("LoseTime");
+			countdownText.text = "Times Up!";
+			SceneManager.LoadScene ("GameOver");
+		}
+	}
+
+	IEnumerator LoseTime()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
+}
